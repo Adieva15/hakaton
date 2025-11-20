9cec6dddd70b30808f98b0d57543b4624bc7d61c
asus <adievadine@gmail.com>
2025-11-20 15:55:23 +0300

    основная команда

---
+++ main.py
@@ -0,0 +1,29 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import message_handlers
+import logging
+import config
+
+def main():
+    try:
+        logger.info("Запуск бота...")
+        application = Application.builder().token(bot_token).build()
+
+        application.add_handler(CommandHandler('start', start))
+        application.add_handler(CommandHandler('help', help_command))
+        application.add_handler(CommandHandler('about', about))
+        application.add_handler(CommandHandler('reset', reset_command))
+
+        # обработка сообщений
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handlemes))
+
+        logger.info("бот запущен!")
+        application.run_polling()
+
+    except Exception as e:
+        logger.error(f"Ошибка: {e}")
+        print("где-то пошла ошибка")
+
+
+if __name__=="__main__":
+    main()
